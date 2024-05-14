d7e747a53ec3477c5c4fcd296a26db1b9b7b78cf
Jebosa Samson <Jebosasamson7@gmail.com>
2024-05-14 08:18:32 -0700

    updated commit

--- index.js
+++ index.js
@@ -6,53 +6,57 @@ let circleThree = document.querySelector('.Three')
 
 let Stopswitch = document.querySelector('.stop')
 let is_StopWitch = true
+let is_SlowWitch = true
+let is_GoWitch = true
 let Slowswitch = document.querySelector('.slow')
 let Goswitch = document.querySelector('.go')
 
 
 
 
-// function handleAddstop(e){
-//     circleOne.style.background = 'red'
-//     is_StopWitch = false
-// }
-
-function handleAllActions(check, backgroundUpdate, backgroundColor){
-    if(check){
-        backgroundUpdate.style.background = backgroundColor
-        check = false
-    }else{
-        backgroundUpdate.style.background = `black`
-        check = true
-    }
-}
-
 function handleStopEvent(){
+    // if(is_StopWitch){
+    //     circleOne.classList.add('test')
+    //     is_StopWitch = false
+    // }else{
+    //     circleOne.classList.remove('')
+    //     is_StopWitch = true
+    // }
     if(is_StopWitch){
-        circleOne.style.background = 'red'
-        is_StopWitch = false
-    }else{
-        circleOne.style.background = 'black'
-        is_StopWitch = true
+        circleOne.classList.toggle('test')
     }
-}
-
-Stopswitch.addEventListener('click', handleAllActions(is_StopWitch, circleOne, 'red'))
-
-
-function handleslow(){
-    circleTwo.style.backgroundColor = 'orange'
-
-}
-function handlego(){
-    circleThree.style.background = 'green'
 
 }
 
+Stopswitch.addEventListener('click',handleStopEvent)
 
-// ADD EVENT
-// Stopswitch.addEventListener('click', handleStopEvent)
+
+function handleslow()
+{
+    if(is_SlowWitch){
+        circleTwo.style.background= 'orange'
+        is_SlowWitch = false
+    }else{
+        circleTwo.style.background = 'black'
+        is_SlowWitch = true
+    }
+
+}
+
 Slowswitch.addEventListener('click', handleslow)
+
+function handlego(){
+    if(is_GoWitch){
+        circleThree.style.background= 'green'
+        is_GoWitch = false
+    }else{
+        circleThree.style.background = 'black'
+        is_GoWitch = true
+    }
+
+}
+
+
 Goswitch.addEventListener('click', handlego)
 
 
